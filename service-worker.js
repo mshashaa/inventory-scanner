bf8293773f8a02ce35d475a1234b0fe81de79fe9
mshashaa <155410938+mshashaa@users.noreply.github.com>
2026-08-15 01:05:14 +0400

    Release v6 service worker

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "inventory-scanner-v5";
+const CACHE_NAME = "inventory-scanner-v6";
 const APP_SHELL = [
   "./",
-  "./index.html?v=5",
-  "./styles.css?v=5",
-  "./app.js?v=5",
-  "./manifest.webmanifest?v=5",
+  "./index.html?v=6",
+  "./styles.css?v=6",
+  "./app.js?v=6",
+  "./manifest.webmanifest?v=6",
   "./icon.svg",
 ];
 
