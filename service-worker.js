eb8fdb57b46676e415ade1e3eac0b11103fc2807
mshashaa <155410938+mshashaa@users.noreply.github.com>
2026-08-15 09:58:15 +0400

    Bump cache to v10

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "inventory-scanner-v9";
+const CACHE_NAME = "inventory-scanner-v10";
 const APP_SHELL = [
   "./",
-  "./index.html?v=9",
-  "./styles.css?v=9",
-  "./app.js?v=9",
-  "./manifest.webmanifest?v=9",
+  "./index.html?v=10",
+  "./styles.css?v=10",
+  "./app.js?v=10",
+  "./manifest.webmanifest?v=10",
   "./icon.svg",
 ];
 
